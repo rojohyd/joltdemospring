0addb4fcd805fd549fbada96b0c7ad3dc31bbb49
v-rohit.j <v-rohit.j@turvo.com>
2021-08-17 13:00:44 +0530

    initial commit

---
+++ src/main/java/com/turvo/joltdemo/JoltdemoApplication.java
@@ -0,0 +1,13 @@
+package com.turvo.joltdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoltdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoltdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/turvo/joltdemo/JoltdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turvo.joltdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoltdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
